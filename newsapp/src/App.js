9c8066ae256e1002446979eed9e518ca9c185e2c
Himanshi <shimanshi9911@gmail.com>
2022-04-03 18:43:05 +0530

    initial commit

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 const alankey = '28eeee6646fb6004381645abb0c9a1322e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -14,7 +14,7 @@ const App = () => {
         }
       }
     })
-  },[])
+  }, [] )
   return (
     <div>
       <h1>Alan AI News App</h1>
